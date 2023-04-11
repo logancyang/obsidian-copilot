79d6c3b8314488bdfbebf3f14c6229ba73e21354
Logan Yang <logancyang@gmail.com>
2023-04-11 12:03:07 -0700

    Add comments for react hooks and local states management

--- src/sharedState.ts
+++ src/sharedState.ts
@@ -30,10 +30,19 @@ export function useSharedState(
   (message: ChatMessage) => void,
   () => void
 ] {
+  // Initializes the local chatHistory state with the current
+  // sharedState chatHistory using the useState hook
+  // setChatHistory is used to update the *local* state
   const [chatHistory, setChatHistory] = useState<ChatMessage[]>(
     sharedState.getMessages()
   );
 
+  // The useEffect hook ensures that the local state is synchronized
+  // with the shared state when the component is mounted.
+  // [] is the dependency array. The effect will only run if one of
+  // the dependencies has changed since the last render.
+  // When there are no dependencies, the effect will only run once,
+  // *right after the initial render* (similar to componentDidMount in class components).
   useEffect(() => {
     setChatHistory(sharedState.getMessages());
   }, []);
