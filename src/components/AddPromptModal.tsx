026a1390c46cb7b24b02263e2484fa3179e599ec
Logan Yang <logancyang@gmail.com>
2023-05-31 20:15:23 -0700

    Add description and styling to add custom prompt modal

--- src/components/AddPromptModal.tsx
+++ src/components/AddPromptModal.tsx
@@ -13,17 +13,50 @@ export class AddPromptModal extends Modal {
       'div',
       { cls: 'custom-prompt-input-container' }
     );
-    titleContainer.createEl('label', { text: 'Prompt Title' });
+
+    titleContainer.createEl(
+      'h3', { text: 'Title', cls: 'custom-prompt-header' }
+    );
+    titleContainer.createEl(
+      'p',
+      {
+        text: 'The title of the prompt, must be unique.',
+        cls: 'custom-prompt-description',
+      }
+    );
+
     const titleField = titleContainer.createEl('input', { type: 'text' });
 
     const promptContainer = formContainer.createEl(
       'div',
       { cls: 'custom-prompt-input-container' }
     );
-    promptContainer.createEl('label', { text: 'Prompt' });
+
+    promptContainer.createEl(
+      'h3', { text: 'Prompt', cls: 'custom-prompt-header' }
+    );
+    promptContainer.createEl(
+      'p',
+      {
+        text: 'The content of the prompt. Use "{}" to represent the selected text. For example, "Improve the readability of the following text: {}"',
+        cls: 'custom-prompt-description',
+      }
+    );
     const promptField = promptContainer.createEl('textarea');
 
-    const saveButton = formContainer.createEl(
+    promptContainer.createEl(
+      'p',
+      {
+        text: 'Save the prompt to the local prompt library. You can then use it with the Copilot command: *Apply custom prompt to selection*.',
+        cls: 'custom-prompt-description',
+      }
+    );
+
+    const saveButtonContainer = formContainer.createEl(
+      'div',
+      { cls: 'custom-prompt-save-btn-container' }
+    );
+    const saveButton = saveButtonContainer.createEl(
       'button',
       { text: 'Save', cls: 'custom-prompt-save-btn' }
     );
@@ -32,7 +65,7 @@ export class AddPromptModal extends Modal {
         onSave(titleField.value, promptField.value);
         this.close();
       } else {
-        new Notice('Please fill in both fields.');
+        new Notice('Please fill in both fields: Title and Prompt.');
       }
     });
   }
