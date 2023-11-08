a407cc9996d297cd3de4b0becaf4f24a11f4d385
Logan Yang <logancyang@gmail.com>
2023-11-07 19:47:02 -0800

    Add new GPT-4 Turbo model

--- src/settings.ts
+++ src/settings.ts
@@ -43,6 +43,7 @@ export class CopilotSettingTab extends PluginSettingTab {
       ChatModelDisplayNames.GPT_35_TURBO,
       ChatModelDisplayNames.GPT_35_TURBO_16K,
       ChatModelDisplayNames.GPT_4,
+      ChatModelDisplayNames.GPT_4_TURBO,
       ChatModelDisplayNames.GPT_4_32K,
       // ChatModelDisplayNames.CLAUDE_1,
       // ChatModelDisplayNames.CLAUDE_1_100K,
@@ -52,6 +53,7 @@ export class CopilotSettingTab extends PluginSettingTab {
       ChatModelDisplayNames.AZURE_GPT_35_TURBO_16K,
       ChatModelDisplayNames.AZURE_GPT_4,
       ChatModelDisplayNames.AZURE_GPT_4_32K,
+      ChatModelDisplayNames.LOCAL_AI,
     ];
 
     new Setting(containerEl)
