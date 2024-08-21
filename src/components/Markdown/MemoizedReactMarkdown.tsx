62caf51f9f58df6363641e63f1516238a3cdb6e8
Logan Yang <logancyang@gmail.com>
2024-08-21 14:54:11 -0700

    Add code syntax highlighting and copy code

--- src/components/Markdown/MemoizedReactMarkdown.tsx
+++ src/components/Markdown/MemoizedReactMarkdown.tsx
@@ -1,5 +1,23 @@
-import { FC, memo } from 'react';
-import ReactMarkdown, { Options } from 'react-markdown';
+import React, { FC, memo } from "react";
+import ReactMarkdown, { Options } from "react-markdown";
+import CodeBlock from "./CodeBlock"; // Adjust the import path as necessary
 
-const MemoizedReactMarkdown: FC<Options> = memo(ReactMarkdown);
+const MemoizedReactMarkdown: FC<Options> = memo((props) => (
+  <ReactMarkdown
+    {...props}
+    components={{
+      code({ node, inline, className, children, ...props }) {
+        const match = /language-(\w+)/.exec(className || "");
+        return !inline && match ? (
+          <CodeBlock language={match[1]} value={String(children).replace(/\n$/, "")} />
+        ) : (
+          <code className={className} {...props}>
+            {children}
+          </code>
+        );
+      },
+    }}
+  />
+));
+
 export default MemoizedReactMarkdown;
