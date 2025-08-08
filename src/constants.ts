a8587a03f216482be9d04c4d519d5b6e1e9da32d
Logan Yang <logancyang@gmail.com>
2025-08-08 08:44:50 -0700

    Refactor BUILTIN_CHAT_MODELS: remove some models

--- src/constants.ts
+++ src/constants.ts
@@ -221,7 +221,6 @@ export const BUILTIN_CHAT_MODELS: CustomModel[] = [
     enabled: true,
     isBuiltIn: true,
     core: true,
-    projectEnabled: true,
     capabilities: [ModelCapability.VISION],
   },
   {
@@ -230,7 +229,6 @@ export const BUILTIN_CHAT_MODELS: CustomModel[] = [
     enabled: true,
     isBuiltIn: true,
     core: true,
-    projectEnabled: true,
     capabilities: [ModelCapability.VISION],
   },
   {
@@ -238,7 +236,7 @@ export const BUILTIN_CHAT_MODELS: CustomModel[] = [
     provider: ChatModelProviders.OPENAI,
     enabled: true,
     isBuiltIn: true,
-    projectEnabled: true,
+    core: true,
     capabilities: [ModelCapability.VISION],
   },
   {
@@ -264,16 +262,7 @@ export const BUILTIN_CHAT_MODELS: CustomModel[] = [
     provider: ChatModelProviders.OPENAI,
     enabled: true,
     isBuiltIn: true,
-    core: true,
     capabilities: [ModelCapability.VISION],
-  },
-  {
-    name: ChatModels.O4_mini,
-    provider: ChatModelProviders.OPENAI,
-    enabled: true,
-    isBuiltIn: true,
-    core: true,
-    capabilities: [ModelCapability.REASONING],
   },
   {
     name: ChatModels.CLAUDE_4_SONNET,
@@ -288,20 +277,6 @@ export const BUILTIN_CHAT_MODELS: CustomModel[] = [
     enabled: true,
     isBuiltIn: true,
     capabilities: [ModelCapability.VISION, ModelCapability.REASONING],
-  },
-  {
-    name: ChatModels.CLAUDE_3_5_SONNET,
-    provider: ChatModelProviders.ANTHROPIC,
-    enabled: true,
-    isBuiltIn: true,
-    core: true,
-    capabilities: [ModelCapability.VISION],
-  },
-  {
-    name: ChatModels.CLAUDE_3_5_HAIKU,
-    provider: ChatModelProviders.ANTHROPIC,
-    enabled: true,
-    isBuiltIn: true,
   },
   {
     name: ChatModels.GROK3,
@@ -330,18 +305,6 @@ export const BUILTIN_CHAT_MODELS: CustomModel[] = [
     isBuiltIn: true,
     projectEnabled: true,
     capabilities: [ModelCapability.VISION],
-  },
-  {
-    name: ChatModels.COMMAND_R,
-    provider: ChatModelProviders.COHEREAI,
-    enabled: true,
-    isBuiltIn: true,
-  },
-  {
-    name: ChatModels.COMMAND_R_PLUS,
-    provider: ChatModelProviders.COHEREAI,
-    enabled: true,
-    isBuiltIn: true,
   },
   {
     name: ChatModels.AZURE_OPENAI,
